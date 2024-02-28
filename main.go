7b120dfa02534edcd2c3ef330980282fc487576c
neukkkn <santyagonarvaezns@gmail.com>
2024-02-28 18:12:15 -0500

    MIS ARANITAS UWU

--- main.go
+++ main.go
@@ -10,16 +10,38 @@ type Alimento struct{
 	Description string `json:"description"`
 }
 
+
+type Arana struct {
+	ID           int     `json:ID`
+	Tipo string `json:ID`
+	Nombre string `json:Nombre`
+}
+
 var alimentos = []Alimento{
 	 {ID: 1, Nombre:"Arepas de choclo", Description: "Hechas de maíz"},
 	{ID: 1, Nombre:"Arepitas", Description: "Hechas de maicitos"},
 	{ID: 1, Nombre:"Buñuelos", Description: "redondos"},
 }
+
+var aranas = []Arana{
+	{ID: 1, Tipo: "Tarantula", Nombre: "kidd keo"},
+	{ID: 2, Tipo: "Aranita chiquita", Nombre: "Ariana Grande"},
+}
+
 func getAlimento(a *gin.Context){
 a.IndentedJSON(http.StatusOK, alimentos)
 }
+
+
+func getAranas(a *gin.Context) {
+	a.IndentedJSON(http.StatusOK, aranas)
+}
+
+
 func main(){
-router := gin.Default()
-router.GET("/alimentos", getAlimento)
-router.Run("localhost:4000")
+	router := gin.Default()
+	router.GET("/alimentos", getAlimento)
+	router.GET("/aranas", getAranas)
+	router.Run("localhost:4000")
+	
 }
