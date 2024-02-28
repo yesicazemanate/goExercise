ff029e7f5a9fddd38f465c0e68950abcc8dc8a14
andreayuliethsalazarperez31@gmail.com <cedelgado454@misena.edu.co>
2024-02-28 18:39:57 -0500

    andrea yulieth salazar

--- main.go
+++ main.go
@@ -1,25 +1,49 @@
 package main
-import(
-"github.com/gin-gonic/gin"
-"net/http"
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
-type Alimento struct{
-	ID int `json:"id"`
-	Nombre string `json:"nombre"`
+type Fruta struct {
+	ID          int    `json:"id"`
+	Nombre      string `json:"nombre"`
 	Description string `json:"description"`
 }
 
-var alimentos = []Alimento{
-	 {ID: 1, Nombre:"Arepas de choclo", Description: "Hechas de maíz"},
-	{ID: 1, Nombre:"Arepitas", Description: "Hechas de maicitos"},
-	{ID: 1, Nombre:"Buñuelos", Description: "redondos"},
+var mangos = []Fruta{
+	{ID: 1, Nombre: "Mango verde", Description: "Es un mango que todavía está verde y no es jugoso"},
+	{ID: 2, Nombre: "Mango rojo", Description: "Es un mango que es muy jugoso ya que su color rojo lo demuestra"},
+	{ID: 3, Nombre: "Mango amarillo", Description: "Es un mango que está entre jugoso y verde"},
 }
-func getAlimento(a *gin.Context){
-a.IndentedJSON(http.StatusOK, alimentos)
+
+func getFruta(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID debe ser un número entero"})
+		return
+	}
+
+	var mango Fruta
+	for _, item := range mangos {
+		if item.ID == id {
+			mango = item
+			break
+		}
+	}
+
+	if mango.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Mango no encontrado"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, mango)
 }
-func main(){
-router := gin.Default()
-router.GET("/alimentos", getAlimento)
-router.Run("localhost:4000")
+
+func main() {
+	router := gin.Default()
+	router.GET("/frutamango/:id", getFruta)
+	router.Run("localhost:5000")
 }
