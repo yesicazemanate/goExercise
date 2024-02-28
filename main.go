6cd2d149f1ffffaa42d543319bd10dad9b483489
neukkkn <santyagonarvaezns@gmail.com>
2024-02-28 18:22:14 -0500

    MIS araNITAS AHORA SE PUDEEN BUSCAR POR ID :spider:

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 import(
 "github.com/gin-gonic/gin"
 "net/http"
+"strconv"
 )
 
 type Alimento struct{
@@ -37,11 +38,36 @@ func getAranas(a *gin.Context) {
 	a.IndentedJSON(http.StatusOK, aranas)
 }
 
+func getAranaByID(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID no válido"})
+		return
+	}
+
+	var arana Arana
+	for _, a := range aranas {
+		if a.ID == id {
+			arana = a
+			break
+		}
+	}
+
+	if arana.Nombre == "" {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Arana no encontrada"})
+		return
+	}
+
+	c.JSON(http.StatusOK, arana)
+}
+
 
 func main(){
 	router := gin.Default()
 	router.GET("/alimentos", getAlimento)
 	router.GET("/aranas", getAranas)
+	router.GET("/aranas/:id", getAranaByID)
 	router.Run("localhost:4000")
 	
 }
