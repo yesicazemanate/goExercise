f5a0702f4e73303bf2546e767d004730c6780b49
neukkkn <santyagonarvaezns@gmail.com>
2024-02-29 13:29:04 -0500

    fix

--- main.go
+++ main.go
@@ -1,65 +1,16 @@
-// package main
-// import(
-// "github.com/gin-gonic/gin"
-// "net/http"
-// )
-
-// type Alimento struct{
-// 	ID int `json:"id"`
-// 	Nombre string `json:"nombre"`
-// 	Description string `json:"description"`
-// }
-
-// var alimentos = []Alimento{
-// 	 {ID: 1, Nombre:"Arepas de choclo", Description: "Hechas de maíz"},
-// 	{ID: 1, Nombre:"Arepitas", Description: "Hechas de maicitos"},
-// 	{ID: 1, Nombre:"Buñuelos", Description: "redondos"},
-// }
-// func getAlimento(a *gin.Context){
-// a.IndentedJSON(http.StatusOK, alimentos)
-// }
-// func main () {
-// router := gin.Default()
-// router.GET("/alimentos", getAlimento)
-// router.Run("localhost:4000")
-// }
-
-// package main
-//  import(
-// "github.com/gin-gonic/gin"
-// "net/http"
-// )
-// type Frutas struct{
-// 	ID int `json:"id"`
-// 	Nombre string `json:"nombre"`
-// 	Description string `json:"description"`
-// }
-// var fruits = []Frutas{
-// 	{ID : 1 , Nombre: "Sandia" , Description: "Fruta tropical"},
-// 	{ID : 2, Nombre: "Limón" , Description: "Fruta ácida"},
-// 	{ID : 3, Nombre: "Naranja", Description: "Fruta semi-ácida"},
-// }
-// func getFruta(f*gin.Context)  {
-// 	f.IndentedJSON(http.StatusOK, fruits)
-
-// }
-// func main () {
-// router := gin.Default()
-// router.GET("/frutas", getFruta)
-// router.Run("localhost:5000")
-// }
-
 package main
-import(
-"github.com/gin-gonic/gin"
-"net/http"
-"strconv"
 
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
 )
+
+type Alimento struct{
+	ID int `json:"id"`
+	Nombre string `json:"nombre"`
+	Description string `json:"description"`
+}
 
 type Frutas struct {
 	ID          int    `json:"id"`
@@ -119,14 +70,6 @@ func getAranaByID(c *gin.Context) {
 }
 
 
-func main(){
-	router := gin.Default()
-	router.GET("/alimentos", getAlimento)
-	router.GET("/aranas", getAranas)
-	router.GET("/aranas/:id", getAranaByID)
-	router.Run("localhost:4000")
-	
-}
 var fruits = []Frutas{
 	{ID: 1, Nombre: "Sandia", Description: "Fruta tropical"},
 	{ID: 2, Nombre: "Limón", Description: "Fruta ácida"},
@@ -140,7 +83,7 @@ func getFrutaByID(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Formato de ID inválido"})
 		return
 	}
-
+	
 	// Encontrar la fruta por ID
 	var frutaEncontrada *Frutas
 	for _, c := range fruits {
@@ -151,19 +94,22 @@ func getFrutaByID(c *gin.Context) {
 			
 		}
 	}
-
+	
 	// Comprobar si se encontró la fruta
-	if frutaEncontrada.ID == nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Fruta no encontrada"})
-		return
-	}
-
+	// if frutaEncontrada.ID == nil {
+	// 	c.JSON(http.StatusNotFound, gin.H{"error": "Fruta no encontrada"})
+	// 	return
+	// }
+	
 	// Devolver la fruta encontrada
 	c.IndentedJSON(http.StatusOK, frutaEncontrada)
 }
 
-func main() {
+
+func main(){
 	router := gin.Default()
-	router.GET("/frutas/:id", getFrutaByID) // Definir ruta con parámetro ":id"
-	router.Run("localhost:6000")
+	router.GET("/alimentos", getAlimento)
+	router.GET("/aranas", getAranas)
+	router.GET("/aranas/:id", getAranaByID)
+	router.Run("localhost:4000")
 }
