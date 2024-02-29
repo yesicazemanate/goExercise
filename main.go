a1825a3e8fe61adf2dffc05680b9e9e356795f59
neukkkn <santyagonarvaezns@gmail.com>
2024-02-29 15:31:54 -0500

    :spider:

--- main.go
+++ main.go
@@ -5,32 +5,15 @@ import(
 "strconv"
 )
 
-type Alimento struct{
-	ID int `json:"id"`
-	Nombre string `json:"nombre"`
-	Description string `json:"description"`
-}
-
-
 type Arana struct {
 	ID           int     `json:ID`
 	Tipo string `json:ID`
 	Nombre string `json:Nombre`
 }
 
-var alimentos = []Alimento{
-	 {ID: 1, Nombre:"Arepas de choclo", Description: "Hechas de maíz"},
-	{ID: 1, Nombre:"Arepitas", Description: "Hechas de maicitos"},
-	{ID: 1, Nombre:"Buñuelos", Description: "redondos"},
-}
-
 var aranas = []Arana{
 	{ID: 1, Tipo: "Tarantula", Nombre: "kidd keo"},
 	{ID: 2, Tipo: "Aranita chiquita", Nombre: "Ariana Grande"},
-}
-
-func getAlimento(a *gin.Context){
-a.IndentedJSON(http.StatusOK, alimentos)
 }
 
 
@@ -68,6 +51,18 @@ func main(){
 	router.GET("/alimentos", getAlimento)
 	router.GET("/aranas", getAranas)
 	router.GET("/aranas/:id", getAranaByID)
+
+
+
+
+
+
+
+
+
+
+
+	
 	router.Run("localhost:4000")
 	
 }
