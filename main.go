252572da76a2d58da52ae79fa00cb23e142f989a
brayan23 <bdelgadomunoz@gmail.com>
2024-02-29 16:09:50 -0500

    clover

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import(
 "net/http"
 "strconv"
 )
+//Brayn Delgado//
+ type Server struct {
+	ID          int
+	Nombre      string
+	Description string
+	// Otros campos que puedas necesitar
+}
 
 //YERSON
 type Oso struct {
@@ -18,7 +25,43 @@ var osos = []Oso{
 	{ID: 2, Nombre: "Oso Polar", Descripcion: "Habita en el Ártico."},
 	{ID: 3, Nombre: "Oso Panda", Descripcion: "Caracterizado por su pelaje blanco y negro."},
 }
+var frutass = []Server{
+	{ID: 1, Nombre: "Manzana", Description: "fruta que se da en climas frios"},
+	{ID: 2, Nombre: "Uva", Description: "fruta dulce"},
+	{ID: 3, Nombre: "Banano", Description: "fruta con mucha proteina"},
+	{ID: 4, Nombre: "Mango", Description: "fruta tropical"},
+}
+func buscarServerPorID(idBuscado int) *Server {
+	for _, server := range frutass {
+		if server.ID == idBuscado {
+			return &server // Retorna una referencia al objeto encontrado
+		}
+	}
+	return nil // Retorna nil si no se encuentra el objeto
+}
 
+func getServerByID(c *gin.Context) {
+	// Obtener el ID del objeto desde la URL
+	idStr := c.Param("id")
+
+	// Convertir el ID de string a int
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID inválido"})
+		return
+	}
+
+	// Buscar el objeto Server por ID
+	serverEncontrado := buscarServerPorID(id)
+
+	// Verificar si el objeto fue encontrado
+	if serverEncontrado != nil {
+		// Devolver el objeto encontrado como JSON
+		c.JSON(http.StatusOK, serverEncontrado)
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Objeto no encontrado"})
+	}
+}
 func getOso(c *gin.Context) {
     c.IndentedJSON(http.StatusOK,osos)
 }
@@ -174,8 +217,7 @@ func main() {
   router.GET("/osos", getOso)
   router.GET("/osos/:id", getOsoPorID)
 
-
-
+	router.GET("/server/:id", getServerByID)
 }
 
 
