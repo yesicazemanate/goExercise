92172090ca001a6f7aa1ae871731c9723ffa065c
ronald13130 <ronaldpino1313@gmail.com>
2024-02-29 13:49:14 -0500

    final

--- main.go
+++ main.go
@@ -1,69 +1,43 @@
-// package main
-// import(
-// "github.com/gin-gonic/gin"
-// "net/http"
-// )
-
-// type Alimento struct{
-// 	ID int `json:"id"`
-// 	Nombre string `json:"nombre"`
-// 	Description string `json:"description"`
-// }
-
-// var alimentos = []Alimento{
-// 	 {ID: 1, Nombre:"Arepas de choclo", Description: "Hechas de maíz"},
-// 	{ID: 1, Nombre:"Arepitas", Description: "Hechas de maicitos"},
-// 	{ID: 1, Nombre:"Buñuelos", Description: "redondos"},
-// }
-// func getAlimento(a *gin.Context){
-// a.IndentedJSON(http.StatusOK, alimentos)
-// }
-// func main(){
-// router := gin.Default()
-// router.GET("/alimentos", getAlimento)
-// router.Run("localhost:4000")
-//
-
-// /Edinson
+// //Edinson
 package main
 
 import (
 	"net/http"
-	
 
 	"github.com/gin-gonic/gin"
 )
-type Arana struct{
-	ID string `json:"id"`
-	Nombre string `json:"nombre"`
+
+type Aranass struct {
+	ID          string `json:"id"`
+	Nombre      string `json:"nombre"`
 	Description string `json:"description"`
 }
 
-var aranas = []Arana{
+var aranass = []Aranass{
 	{ID: "1", Nombre: "Tarantulas", Description: "arañas migalomorfas de gran tamaño con el cuerpo cubierto por pelos llamados sedas."},
-	{ID: "2", Nombre: "Viudas Negras", Description:"tiene un cuerpo negro y brillante con una forma de reloj de arena rojo en la zona ventral"},
-	{ID: "3", Nombre: "Licósidos", Description:"arañas que vagan en el suelo, excavando pequeñas galerías verticales u ocupando grietas naturales desde las que acechan a sus presas, cuya presencia detectan por las vibraciones del suelo."},
-}
-func getArana(a*gin.Context){
-	a.IndentedJSON(http.StatusOK,aranas)
+	{ID: "2", Nombre: "Viudas Negras", Description: "tiene un cuerpo negro y brillante con una forma de reloj de arena rojo en la zona ventral"},
+	{ID: "3", Nombre: "Licósidos", Description: "arañas que vagan en el suelo, excavando pequeñas galerías verticales u ocupando grietas naturales desde las que acechan a sus presas, cuya presencia detectan por las vibraciones del suelo."},
 }
 
-func getAranaByID(c *gin.Context){
-	id := c.Param ("id")
-	for _,a := range aranas{
-		if a.ID == id{
-			c.IndentedJSON(http.StatusOK,a)
+func getAranass(a *gin.Context) {
+	a.IndentedJSON(http.StatusOK, aranass)
+}
+
+func getAranassByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range aranass {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
 }
-	
+func main() {
+	router := gin.Default()
+	router.GET("/aranass", getAranass)
+	router.GET("/aranass/:id", getAranassByID)
+	router.Run("localhost:4000")
 
-
-func main(){
-	router:=gin.Default()
-	router.GET("/aranas",getArana)
-	router.GET("/aranas/:id", getAranaByID)
-	router.Run("localhost:5000")
-	
 }
+
+///Edinson
