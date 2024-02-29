03a8e179e09e01cc6968565abc829614389822aa
andreayuliethsalazarperez31@gmail.com <cedelgado454@misena.edu.co>
2024-02-29 13:53:57 -0500

    andrea

--- main.go
+++ main.go
@@ -112,11 +112,59 @@ func getFrutaByID(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, frutaEncontrada)
 }
 
+//Andrea salazar perez
+type Fruta struct {
+	ID          int    `json:"id"`
+	Nombre      string `json:"nombre"`
+	Description string `json:"description"`
+}
+
+var mangos = []Fruta{
+	{ID: 1, Nombre: "Mango verde", Description: "Es un mango que todavía está verde y no es jugoso"},
+	{ID: 2, Nombre: "Mango rojo", Description: "Es un mango que es muy jugoso ya que su color rojo lo demuestra"},
+	{ID: 3, Nombre: "Mango amarillo", Description: "Es un mango que está entre jugoso y verde"},
+}
+
+func getFruta(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID debe ser un número entero"})
+		return
+	}
+
+	var mango Fruta
+	for _, item := range mangos {
+		if item.ID == id {
+			mango = item
+			break
+		}
+	}
+
+	if mango.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Mango no encontrado"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, mango)
+}
+
+func getAll(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, mangos)
+}
+
+//Andrea salazar perez
 
 func main(){
 	router := gin.Default()
 	router.GET("/alimentos", getAlimento)
 	router.GET("/aranas", getAranas)
 	router.GET("/aranas/:id", getAranaByID)
+	router.GET("/frutamango/:id", getFruta)
+	router.GET("/frutamango", getAll)
 	router.Run("localhost:4000")
 }
+
+
+
+
