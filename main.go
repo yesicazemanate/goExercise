b05b0d967d7ba88fe01a9eb0763f187300bf6a0a
sebasbn29 <sebastianburbano06@gmail.com>
2024-02-29 16:04:07 -0500

    Serpientes

--- main.go
+++ main.go
@@ -6,16 +6,40 @@ import (
 	"strconv"
 )
 
+type Serpiente struct {
+	ID     int    `json:ID`
+	Tipo   string `json:"Tipo"`
+	Nombre string `json:"Nombre"`
+}
+
+var serpiente = []Serpiente{
+	{ID: 1, Tipo: "Cobra Real", Nombre: "Ophiophagus"},
+	{ID: 2, Tipo: "Mamba Negra ", Nombre: "Dendroaspis "},
+}
+
+func getSerpiente(s *gin.Context) {
+	s.IndentedJSON(http.StatusOK, serpiente)
+}
+
+func getSerpienteByID(s *gin.Context) {
+	idse := s.Param("id")
+	idint, err := strconv.Atoi(idse)
+	if err != nil {
+		s.IndentedJSON(http.StatusNotFound, gin.H{"message": "Serpiente no encontrada"})
+	}
+
+	for _, e := range serpiente {
+		if e.ID == idint {
+			s.IndentedJSON(http.StatusOK, e)
+		}
+	}
+}
+
 func main() {
 	router := gin.Default()
 
-
-
-
-
-
-
-
+	router.GET("/serpiente", getSerpiente)
+	router.GET("/serpiente/:id", getSerpienteByID)
 
 	router.Run("localhost:4000")
 }
