13b5b16fae165ef626c499a466cdbb03e6748bba
ronald13130 <ronaldpino1313@gmail.com>
2024-02-29 15:41:13 -0500

    spider

--- main.go
+++ main.go
@@ -7,25 +7,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Aranass struct {
+type Spider struct {
 	ID          string `json:"id"`
 	Nombre      string `json:"nombre"`
 	Description string `json:"description"`
 }
 
-var aranass = []Aranass{
+var spider = []Spider{
 	{ID: "1", Nombre: "Tarantulas", Description: "arañas migalomorfas de gran tamaño con el cuerpo cubierto por pelos llamados sedas."},
 	{ID: "2", Nombre: "Viudas Negras", Description: "tiene un cuerpo negro y brillante con una forma de reloj de arena rojo en la zona ventral"},
 	{ID: "3", Nombre: "Licósidos", Description: "arañas que vagan en el suelo, excavando pequeñas galerías verticales u ocupando grietas naturales desde las que acechan a sus presas, cuya presencia detectan por las vibraciones del suelo."},
 }
 
-func getAranass(a *gin.Context) {
-	a.IndentedJSON(http.StatusOK, aranass)
+func getSpider(a *gin.Context) {
+	a.IndentedJSON(http.StatusOK, spider)
 }
 
-func getAranassByID(c *gin.Context) {
+func getSpiderByID(c *gin.Context) {
 	id := c.Param("id")
-	for _, a := range aranass {
+	for _, a := range spider {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
@@ -34,8 +34,8 @@ func getAranassByID(c *gin.Context) {
 }
 func main() {
 	router := gin.Default()
-	router.GET("/aranass", getAranass)
-	router.GET("/aranass/:id", getAranassByID)
+	router.GET("/spider", getSpider)
+	router.GET("/spider/:id", getSpiderByID)
 	router.Run("localhost:4000")
 
 }
