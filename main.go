857d8b505875a3e8423a5297aa9101fe9bb58cdf
ronald13130 <ronaldpino1313@gmail.com>
2024-02-29 16:39:07 -0500

    edinson

--- main.go
+++ main.go
@@ -339,7 +339,39 @@ func getNaranjaByID(c *gin.Context) {
 }
 
 //Luisa Villacorte
+//edinson
+type Spider struct {
+	ID          string `json:"id"`
+	Nombre      string `json:"nombre"`
+	Description string `json:"description"`
+}
 
+var spider = []Spider{
+	{ID: "1", Nombre: "Tarantulas", Description: "arañas migalomorfas de gran tamaño con el cuerpo cubierto por pelos llamados sedas."},
+	{ID: "2", Nombre: "Viudas Negras", Description: "tiene un cuerpo negro y brillante con una forma de reloj de arena rojo en la zona ventral"},
+	{ID: "3", Nombre: "Licósidos", Description: "arañas que vagan en el suelo, excavando pequeñas galerías verticales u ocupando grietas naturales desde las que acechan a sus presas, cuya presencia detectan por las vibraciones del suelo."},
+}
+
+func getSpider(a *gin.Context) {
+	a.IndentedJSON(http.StatusOK, spider)
+}
+
+func getSpiderByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range spider {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+}
+
+
+	
+	
+
+
+//edinson
 
 func main() {
 	router := gin.Default()
@@ -362,6 +394,8 @@ func main() {
   	router.GET("/serpiente", getSerpiente)
 	router.GET("/serpiente/:id", getSerpienteByID)
   	
+	router.GET("/spider", getSpider)
+	router.GET("/spider/:id", getSpiderByID)
 
   
   
