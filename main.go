fac4fa7e8d0d15bdb8eb2a3965e44a5d5658a121
ronald13130 <ronaldpino1313@gmail.com>
2024-02-29 14:59:49 -0500

    kjk

--- main.go
+++ main.go
@@ -408,6 +408,35 @@ func getOsoPorID(c *gin.Context) {
 
 	c.JSON(http.StatusNotFound, gin.H{"error": "Oso no encontrado"})
 }
+
+///edinson
+type Aranass struct {
+	ID          string `json:"id"`
+	Nombre      string `json:"nombre"`
+	Description string `json:"description"`
+}
+
+var aranass = []Aranass{
+	{ID: "1", Nombre: "Tarantulas", Description: "arañas migalomorfas de gran tamaño con el cuerpo cubierto por pelos llamados sedas."},
+	{ID: "2", Nombre: "Viudas Negras", Description: "tiene un cuerpo negro y brillante con una forma de reloj de arena rojo en la zona ventral"},
+	{ID: "3", Nombre: "Licósidos", Description: "arañas que vagan en el suelo, excavando pequeñas galerías verticales u ocupando grietas naturales desde las que acechan a sus presas, cuya presencia detectan por las vibraciones del suelo."},
+}
+
+func getAranass(a *gin.Context) {
+	a.IndentedJSON(http.StatusOK, aranass)
+}
+
+func getAranassByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range aranass {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+}
+
+///edinson
 // Yerson Fingit
 
 func main() {
@@ -423,7 +452,9 @@ func main() {
 	router.GET("/animales", getAnimales)
 	router.GET("/animales/:id", getAnimal)
 
-
+	router.GET("/aranass", getAranass)
+	router.GET("/aranass/:id", getAranassByID)
+	router.Run("localhost:4000")
 
 	router.GET("/frutas/:id", getFrutaByIDD)
 	router.GET("/ave", getAve)
